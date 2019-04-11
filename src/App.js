4f912df32a47d78f0e98db4324ff5ded78296ff1
mariawenrose <mariarose.wen@gmail.com>
2019-04-12 08:02:08 +1200

    undefined error in ContactsList.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ContactsList from './ContactsList'
 import './App.css';
 
-let contacts = [{
+let contacts = [
+{
   name: 'Scott',
   phone: '555 222 111'
 }, 
@@ -14,21 +15,29 @@ let contacts = [{
   name: 'Koda',
   phone: '444 111 333'
 },
- {
+{
   name: 'Lorenz',
   phone: '888 777 111'
-}]
+}
+];
 
 // console.log(this.props.contact)
 
 const title = "live search test";
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    myContactsFromApp: contacts
+  };
+}
+
   render() {
     return (
       <div className="App backgroundColour">
        <h1>Contacts List</h1>
-       <ContactsList />
+       <ContactsList myContactsFromApp = {this.state.myContactsFromApp} />
       </div>
     );
   }
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -2,14 +2,40 @@ import React from 'react';
 import Contact from './Contact'
 
 class ContactsList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch = (event) => {
+    this.setState({search: event.target.value.substr(0,20)})
+    //setting character count
+  }
+
   render () {
-var contacts = {contacts}
+    let filteredContacts = this.props.contacts.filter(
+      (contact) => {
+        return contacts.name.indexOf(this.state.search) !== -1;
+      }
+    );
+    console.log(this.props.ContactsFromApp)
+
+    var contacts = {contacts}
     return (
+    <div>
+
       <ul>
-        {this.props.contacts.map((contact)=> {
+        {filteredContacts.map((contact)=> {
           return <Contact contact = {contact} />
         })}
       </ul>
+      <input type = "text" placeholder = "search" value = {this.state.search}
+       onChange = {this.updateSearch.bind(this)}
+     />
+   
+    </div>
     )
   }
 }
